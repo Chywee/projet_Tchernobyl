46df0706f2430a80e2c1f401d11f7f8399d9b8b9
Chywee <142968883+Chywee@users.noreply.github.com>
2024-11-23 16:24:32 +0100

    Update tchernobyl_edo_solver.py

--- tchernobyl_edo_solver.py
+++ tchernobyl_edo_solver.py
@@ -1,8 +1,7 @@
 
+#beepboop of mozerfroking maphys hard coded baby B)
 
-from math import sqrt
 
-import matplotlib
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
@@ -17,7 +16,7 @@ sigma_x = 2.65*10 ** (-18)
 maj_sigma_f = 0.0984
 maj_sigma_u = 0.1355
 psi_de_t = 3*10 ** (13)
-end_time = 172800
+end_time = 178200
 def maj_sigma_x (sigma_x,grandX):
     return sigma_x*grandX
 #euler
@@ -31,32 +30,32 @@ def euler(derivatives, x, y, x_stop, h):
         Y.append(y)
     return np.array(X), np.array(Y)
 #edo non résolue
-def dérivé_I(I, t):
+def dérivé_I(I):
     return np.array(gamma_i * maj_sigma_f * psi_de_t - lambda_i * I - sigma_i * I * psi_de_t)
 
-def dérivé_X(X, I, t):
-    return np.array(gamma_x * maj_sigma_f * psi_de_t - lambda_i * I - sigma_x * X * psi_de_t - lambda_x * X)
-
+def dérivé_grandX(X, I):
+    return np.array(gamma_x * maj_sigma_f * psi_de_t - lambda_i * I - sigma_x * grandX * psi_de_t - lambda_x * grandX)
+#ou est le t pour les deux edo non résolue ?? 
 def run_simuI():
-    #"""Run simulation and output times and I"""
+    #Run simulation and output times and I
     start_time = 0
     I0 = 0
     step = 3600
 
     times, I = euler(dérivé_I, start_time, I0 , end_time, step)
-    Is = I[]
+    Is = I#a modifier
 
 
     return times, Is
 
-def run_simuX():
-    #"""Run simulation and output times and I"""
+def run_simugrandX():
+    #Run simulation and output times and X
     start_time = 0
     X0 = 0
     step = 3600
 
-    times, X = euler(dérivé_X, start_time, X0 , end_time, step)
-    Xs = X[]
+    times, grandX = euler(dérivé_grandX, start_time, X0 , end_time, step)
+    Xs = grandX#a modifier
 
 
     return times, Xs
@@ -69,7 +68,7 @@ def plot_simuI(times, Is):
 
     plt.show()
 
-def plot_simuX(times, Xs):
+def plot_simugrandX(times, Xs):
     """Generate plot from the simulation"""
     plt.plot(times, Xs[:, 0], label="X")
     
@@ -81,6 +80,6 @@ def plot_simuX(times, Xs):
 
 if __name__ == "__main__":
     outI = run_simuI()
-    outX = run_simuX()
-    plot_simuX(*outX)
+    outX = run_simugrandX()
+    plot_simugrandX(*outX)
     plot_simuI(*outI)
