b7d4d7e86c1a5847b843696d1958bf43cdb24ada
Chywee <142968883+Chywee@users.noreply.github.com>
2024-11-24 10:45:34 +0100

    working edo solver for tchernobyl edo

--- tchernobyl_edo_solver.py
+++ tchernobyl_edo_solver.py
@@ -1,85 +1,79 @@
+#hard coded maphys things bb 
 
-#beepboop of mozerfroking maphys hard coded baby B)
-
-
-import matplotlib.animation as animation
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.integrate import solve_ivp
-#definition des constente
+
+# Constants
 gamma_i = 0.064
 gamma_x = 0.004
-lambda_i = 2.926*10 ** (-5)
-lambda_x = 2.0996*10 ** (-5)
-sigma_i = 7*10 ** (-24)
-sigma_x = 2.65*10 ** (-18)
+lambda_i = 2.926e-5
+lambda_x = 2.0996e-5
+sigma_i = 7e-24
+sigma_x = 2.65e-18
 maj_sigma_f = 0.0984
 maj_sigma_u = 0.1355
-psi_de_t = 3*10 ** (13)
-end_time = 178200
-def maj_sigma_x (sigma_x,grandX):
-    return sigma_x*grandX
-#euler
-def euler(derivatives, x, y, x_stop, h):
-    X = [x]
-    Y = [y]
-    while x < x_stop:
-        y = y + h * derivatives(x, y)
-        x = x + h
-        X.append(x)
-        Y.append(y)
-    return np.array(X), np.array(Y)
-#edo non résolue
-def dérivé_I(I):
-    return np.array(gamma_i * maj_sigma_f * psi_de_t - lambda_i * I - sigma_i * I * psi_de_t)
+psi_de_t = 3e13
+end_time = 178200  # seconds
 
-def dérivé_grandX(X, I):
-    return np.array(gamma_x * maj_sigma_f * psi_de_t - lambda_i * I - sigma_x * grandX * psi_de_t - lambda_x * grandX)
-#ou est le t pour les deux edo non résolue ?? 
+def maj_sigma_x(sigma_x, grandX):
+    return sigma_x * grandX
+
+# Euler Method
+def euler(derivative, t0, y0, t_end, dt):
+    t = t0
+    y = y0
+    times = [t]
+    values = [y]
+    while t < t_end:
+        y = y + dt * derivative(t, y)
+        t += dt
+        times.append(t)
+        values.append(y)
+    return np.array(times), np.array(values)
+
+# Differential Equations
+def dérivé_I(t, I):
+    return gamma_i * maj_sigma_f * psi_de_t - lambda_i * I - sigma_i * I * psi_de_t
+
+def dérivé_grandX(t, grandX):
+    return gamma_x * maj_sigma_f * psi_de_t - lambda_x * grandX - sigma_x * grandX * psi_de_t
+
+# Simulations
 def run_simuI():
-    #Run simulation and output times and I
-    start_time = 0
-    I0 = 0
-    step = 3600
-
-    times, I = euler(dérivé_I, start_time, I0 , end_time, step)
-    Is = I#a modifier
-
-
-    return times, Is
+    t0 = 0
+    I0 = 0  # Initial condition for I
+    dt = 3600  # Time step in seconds
+    times, I = euler(dérivé_I, t0, I0, end_time, dt)
+    return times, I
 
 def run_simugrandX():
-    #Run simulation and output times and X
-    start_time = 0
-    X0 = 0
-    step = 3600
+    t0 = 0
+    X0 = 0  # Initial condition for X
+    dt = 3600  # Time step in seconds
+    times, X = euler(dérivé_grandX, t0, X0, end_time, dt)
+    return times, X
 
-    times, grandX = euler(dérivé_grandX, start_time, X0 , end_time, step)
-    Xs = grandX#a modifier
-
-
-    return times, Xs
-
+# Plotting Functions
 def plot_simuI(times, Is):
-    #"""Generate plot from the simulation"""
-    plt.plot(times, Is[:, 0], label="I")
-    
+    plt.plot(times / 3600, Is, label="I (particles)")
+    plt.xlabel("Time (hours)")
+    plt.ylabel("I")
+    plt.title("Simulation of I over time")
     plt.legend()
-
+    plt.grid()
     plt.show()
 
 def plot_simugrandX(times, Xs):
-    #"""Generate plot from the simulation"""
-    plt.plot(times, Xs[:, 0], label="X")
-    
+    plt.plot(times / 3600, Xs, label="X (particles)")
+    plt.xlabel("Time (hours)")
+    plt.ylabel("X")
+    plt.title("Simulation of X over time")
     plt.legend()
-
+    plt.grid()
     plt.show()
 
-
-
 if __name__ == "__main__":
-    outI = run_simuI()
-    outX = run_simugrandX()
-    plot_simugrandX(*outX)
-    plot_simuI(*outI)
+    times_I, Is = run_simuI()
+    times_X, Xs = run_simugrandX()
+    plot_simuI(times_I, Is)
+    plot_simugrandX(times_X, Xs)
